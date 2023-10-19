9a7fc89ed6fc272a901b577ed2ddacff84691f05
anshnarula5 <anshnarula55@gmail.com>
2023-10-19 17:23:08 +0530

    first commit

---
+++ backend/src/main/java/com/precizeassignment/PrecizeassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.precizeassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecizeassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecizeassignmentApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/precizeassignment/PrecizeassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.precizeassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecizeassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
